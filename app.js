b54ddc775f7395a123658cd1a56374e129a65f54
Lohith-Varma <dklohithvarma@gmail.com>
2025-09-16 23:53:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach(button =>  {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML=='AC') {
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML=='DEL') {
+            string =    string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.key >= '0' && e.key <='9' || ['+', '-', '*', '/', '.', '%'].includes(e.key)) {
+        string += e.key;
+        input.value = string;
+    }
+    else if(e.key == 'Enter') {
+        string = eval(string);
+        input.value = string;
+    }
+    else if(e.key == 'Backspace') {
+        string = string.substring(0, string.length-1);
+        input.value = string;
+    }
+    else if(e.key == 'Escape') {
+        string = "";
+        input.value = string;
+    }
+})
